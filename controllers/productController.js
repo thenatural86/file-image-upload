407c9091f47817997af536f8dd92d715b84b2074
thenatural86 <philip.smalls86@gmail.com>
2024-04-07 14:04:14 -0300

    file upload package

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,7 +2,7 @@ const Product = require('../models/Product')
 const { StatusCodes } = require('http-status-codes')
 
 const createProduct = async (req, res) => {
-  console.log(req.body)
+  // console.log(req.body)
   const product = await Product.create(req.body)
   res.status(StatusCodes.CREATED).json({ product })
 }
