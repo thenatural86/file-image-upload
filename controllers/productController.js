3eb19151c1e475b9fbdccc4a3bb2f179723a7656
thenatural86 <philip.smalls86@gmail.com>
2024-04-07 17:01:27 -0300

    adds additional checks to image upload

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,13 +2,11 @@ const Product = require('../models/Product')
 const { StatusCodes } = require('http-status-codes')
 
 const createProduct = async (req, res) => {
-  // console.log(req.body)
   const product = await Product.create(req.body)
   res.status(StatusCodes.CREATED).json({ product })
 }
 
 const getAllProducts = async (req, res) => {
-  // res.send('Get all products')
   const products = await Product.find({})
   res.status(StatusCodes.OK).json({ products })
 }
--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,15 +1,32 @@
 const path = require('path')
+const CustomError = require('../errors')
 
 const { StatusCodes } = require('http-status-codes')
 
 const uploadProductImage = async (req, res) => {
+  // check if file exist
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No file Uploaded')
+  }
+
   const productImage = req.files.image
+  // check format
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please Upload Image')
+  }
+
+  // check size
+  const maxSize = 1024 * 1024
+  if (productImage.maxSize > maxSize) {
+    throw new CustomError.BadRequestError(
+      'Please upload image smaller than 1MB'
+    )
+  }
 
   const imagePath = path.join(
     __dirname,
     '../public/uploads/' + `${productImage.name}`
   )
-  // console.log(productImage)
   await productImage.mv(imagePath)
   return res
     .status(StatusCodes.OK)
